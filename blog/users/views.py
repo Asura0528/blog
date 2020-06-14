750e73d2a68b61cf3a5cd5060027482fc1ba9057
Asura0528 <875778210@qq.com>
2020-06-14 14:04:30 +0800

    注册功能实现

--- blog/users/views.py
+++ blog/users/views.py
@@ -7,18 +7,77 @@ from django_redis import get_redis_connection
 from utils.response_code import RETCODE
 from random import randint
 from libs.yuntongxun.sms import CCP
+from users.models import User
+from django.db import DatabaseError
 import logging
+import re
 logger = logging.getLogger('django')
 
 
 # 注册视图
 class RegisterView(View):
-    def get(self, request):
+
+    @staticmethod
+    def get(request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        """
+        1、接收参数
+        2、验证参数
+            2.1 参数是否齐全
+            2.2 手机号格式是否正确
+            2.3 密码格式是否正确
+            2.4 密码和确认密码要一致
+            2.5 短信验证码是否和redis中的一致
+        3、保存注册信息
+        4、返回跳转页面
+        :param request:
+        :return:
+        """
+        # 1、接收参数
+        mobile = request.POST.get('mobile')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        smscode = request.POST.get('sms_code')
+        # 2、验证参数
+        #     2.1 参数是否齐全
+        if not all([mobile, password, password2, smscode]):
+            return HttpResponseBadRequest('缺少必要的参数')
+        #     2.2 手机号格式是否正确
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return HttpResponseBadRequest('手机号不符合规则')
+        #     2.3 密码格式是否正确
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return HttpResponseBadRequest('请输入8-20位密码，密码可以是数字，字母')
+        #     2.4 密码和确认密码要一致
+        if password != password2:
+            return HttpResponseBadRequest('两次密码不一致')
+        #     2.5 短信验证码是否和redis中的一致
+        redis_conn = get_redis_connection('default')
+        redis_sms_code = redis_conn.get('sms:%s' % mobile)
+        if redis_sms_code is None:
+            return HttpResponseBadRequest('短信验证码已过期')
+        if smscode != redis_sms_code.decode():
+            return HttpResponseBadRequest('短信验证码不一致')
+        # 3、保存注册信息
+        # creat_user 可以使用系统的方法对密码进行加密
+        try:
+            user = User.objects.create_user(username=mobile,
+                                            mobile=mobile,
+                                            password=password)
+        except DatabaseError as e:
+            logger.error(e)
+            return HttpResponseBadRequest('注册失败')
+        # 4、返回跳转页面
+        # 暂时返回一个注册成功的信息，后期再实现跳转到指定页面
+        return HttpResponse('注册成功，重定向到首页')
 
 
 class ImageCodeView(View):
-    def get(self, request):
+
+    @staticmethod
+    def get(request):
         """
         1.传递前端传递过来的uuid
         2.判断uuid是否获取成功
@@ -50,7 +109,8 @@ class ImageCodeView(View):
 
 class SmsCodeView(View):
 
-    def get(self, request):
+    @staticmethod
+    def get(request):
         """
         1.接收参数
         2.参数的验证
