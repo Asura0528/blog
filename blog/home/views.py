5881625583c97872820a28fd60ae02b69f984fc3
Asura0528 <875778210@qq.com>
2020-06-22 17:56:31 +0800

    首页数据展示

--- blog/home/views.py
+++ blog/home/views.py
@@ -1,11 +1,59 @@
 from django.shortcuts import render
 from django.views import View
+from home.models import ArticleCategory, Ariticle
+from django.core.paginator import Paginator, EmptyPage
+from django.http.response import HttpResponseNotFound
 
 # Create your views here.
 
 
 # 首页视图
 class IndexView(View):
+    @staticmethod
+    def get(request):
+        """
+        1.获取所有分类信息
+        2.接收用户点击的分类id
+        3.根据分类id进行分类的查询
+        4.获取分页参数
+        5.根据分类信息查询文章数据
+        6.创建分页器
+        7.进行分页处理
+        8.组织数据传递给模板
+        :param request:
+        :return:
+        """
+        # 1.获取所有分类信息
+        categories = ArticleCategory.objects.all()
+        # 2.接收用户点击的分类id
+        cat_id = request.GET.get('cat_id', 1)
+        # 3.根据分类id进行分类的查询
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseNotFound('没有此分类')
+        # 4.获取分页参数
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+        # 5.根据分类信息查询文章数据
+        articles = Ariticle.objects.filter(category=category)
+        # 6.创建分页器
+        paginator = Paginator(articles, per_page=page_size)
+        # 7.进行分页处理
+        try:
+            page_articles = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('empty page')
+        # 总页数
+        total_page = paginator.num_pages
 
-    def get(self, request):
-        return render(request, 'index.html')
+        # 8.组织数据传递给模板
+        context = {
+            'categories': categories,
+            'category': category,
+            'articles': page_articles,
+            'page_size': page_size,
+            'total_page': total_page,
+            'page_num': page_num
+        }
+        return render(request, 'index.html', context=context)
