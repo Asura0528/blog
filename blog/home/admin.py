04694f86630be0fd386ddab7c9948b52dc4993af
Asura0528 <875778210@qq.com>
2020-07-02 12:17:38 +0800

    实现后台管理评论和博客

--- blog/home/admin.py
+++ blog/home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from home.models import ArticleCategory
+from home.models import ArticleCategory, Comment, Ariticle
 # Register your models here.
 
 admin.site.register(ArticleCategory)
+admin.site.register(Comment)
+admin.site.register(Ariticle)
